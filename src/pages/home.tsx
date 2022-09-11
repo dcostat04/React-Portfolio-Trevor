fb17395ca0eb746a33411521acdbdfa97ebfd090
Trevor Aaron Robert Dcosta <63400592+dcostat04@users.noreply.github.com>
2022-09-11 23:03:03 +0530

    Learning about React, TypeScript and Tailwind-CSS

---
+++ src/pages/home.tsx
@@ -0,0 +1,16 @@
+import "./../styles/styles.css";
+export default function Home() {
+  return (
+    <div>
+      <div
+        className=" Background bg-cover  bg-center bg-no-repeat"
+        style={{
+          backgroundImage: "url('/img/trevPhoto.jpg')",
+        }}
+      >
+        <h1 className="text-center"> Trevor Aaron Robert Da Costa</h1>
+        <h1 className="text-center"> Computer Engineer</h1>
+      </div>
+    </div>
+  );
+}
